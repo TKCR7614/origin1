82465c6b2b84c867d8857ae036ab6ea1049c9e79
32941 <3294157416@qq.com>
2024-04-14 23:14:56 +0100

    first version

---
+++ 1.py
@@ -0,0 +1,10 @@
+def Euclidean(a,b):
+    if a==0: # Set up the base case that if a = 0 then GCD(a,b)=b, since the GCD(0,b)=b, and we can stop.
+        return b
+    elif b==0: # Set up the base case that If b = 0 then GCD(a,b)=a, since the GCD(a,0)=a, and we can stop.
+        return a
+    else:
+        c=a%b # If both a and b not equal to 0 then we need recursive call to find the greatest common divisor of b and the remainder of a divied by b, because the greatest common divisor of and b equal to greatest common divisor of  b and the remainder of a divied by b. And c is the remainder of a divided by b.
+        return Euclidean(b,c)
+
+
